32faa87ab942297685b915d7f5e77d311c316230
Sebastian Gomez <seagomezar@gmail.com>
2014-09-04 17:14:38 -0500

    MixPanel and GoogleAnalytics Added.

--- app/views/layouts/default-angular.blade.php
+++ app/views/layouts/default-angular.blade.php
@@ -33,7 +33,13 @@
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 
+	<!-- start Mixpanel -->
+
+	<script type="text/javascript">(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+	for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+	mixpanel.init("02b693fb4dcbd5acc87697d494112164");</script>
 	
+	<!-- end Mixpanel -->
 	</head>
 
 	<body id="page">
@@ -111,6 +117,16 @@
 		<script src="{{ asset('js/app.js') }}"></script> <!-- load our application -->
 		<script src="{{ asset('js/scripts.js') }}"></script> <!-- load our custom scripts -->
         
-		
+	<!-- Start Google Analytics -->
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-54490148-1', 'auto');
+		ga('send', 'pageview');
+		</script>
+	<!-- End Google Analytics -->	
 	</body>
 </html>

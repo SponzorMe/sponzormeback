1af136cfaabbf88d1b60a8e503d851ce013f8f3d
Sebastian Gomez <seagomezar@gmail.com>
2014-11-12 19:11:37 -0500

    Donde esta el error.

--- public/dashboard/js/dashboard.js
+++ public/dashboard/js/dashboard.js
@@ -367,6 +367,7 @@ function settingsController($scope,$Cookie,Customization)
             });
         }            
     });
+	//ACA EMPIEZA EL PROBLEMA
     $scope.gato = function(e)
     {
         alert("sisas");

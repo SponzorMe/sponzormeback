6c2340971e3ed383a1b5b9c5183c66919afc32a4
root <ing.carlosandresrojas@gmail.com>
2015-01-17 14:14:31 -0500

    cambios en produccion

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,10 +26,9 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
     'local' => array('localhost','127.0.0.1','Casa','Sebastian'),
-    'staging' => array('staging.sponzor.me','staging.*','server.sponzor.me','sponzorme'),
-    'production'=>array('www.sponzor.me')
+    'staging' => array('staging.sponzor.me','staging.*','server.sponzor.me'),
+    'production'=>array('www.sponzor.me','sponzorme')
 ));
-
 /*
 |--------------------------------------------------------------------------
 | Bind Paths

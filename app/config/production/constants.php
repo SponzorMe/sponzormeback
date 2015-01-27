8b2a03fec40dd1ba6e29cb9d68fc24e9da79e2fc
Sebastian Gomez <seagomezar@gmail.com>
2015-01-27 12:03:41 -0500

    Corregida la vuelta

--- app/config/production/constants.php
+++ app/config/production/constants.php
@@ -4,7 +4,7 @@ return array(
 	'eventbriteClientSecret'=>'V72EKSC2YWR5Y4XKVKCUL4W45ZAAVXJSEG3KOBAFIVKR6ESIX5',
 	'eventbriteRedirectUrl'=>'https://www.sponzor.me/public/api/v1/connectEvenbrite',//Setear esto en la API
 	'meetupClientId'=>'lc876qakj5itnsnebm3dijus12',
-	'meetupClientSecret'=>'t9lpatu023l44gjbtkno2a9ku9aa',
+	'meetupClientSecret'=>'t9lpatu023l44gjbtkno2a9ku9',
 	'meetupRedirectUrl'=>'https://www.sponzor.me/public/api/v1/connectMeetup'//Setear esto en la API
 	);
 ?>

ffb1ab992b8bed174f6c877ece81d54aa46b2aa2
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-03-16 17:44:10 -0600

    privacy

---
+++ app/lang/es/privacy.php
@@ -0,0 +1,68 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Repositiory Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	'headprivacy' 		=>  "Política de privacidad",
+
+	'head2privacy' 		=>	"Acerca de SponzorMe:",
+
+	'head3privacy'		=>	"Nuestra política:",
+
+  'head4privacy' 		=>  "Consentimiento:",
+
+	'head5privacy' 		=>	"Información que recopilamos:",
+
+	'head6privacy'		=>	"1. Datos personales:",
+
+  'head7privacy' 		=>  "2. Datos no personales::",
+
+	'head8privacy' 		=>	"3. Cookies, etiquetas de píxel, objetos locales compartidos, almacenamiento web y tecnología similares:",
+
+	'head9privacy'		=>	"El uso que hacemos de la información recopilada:",
+
+  'head10privacy' 		=>  "1. Datos personales:",
+
+	'head11privacy' 		=>	"2. Datos no personales:",
+
+	'head12privacy'		=>	"Divulgación de la información recopilada:",
+
+  'head13privacy' 		=>  "1. Datos personales:",
+
+	'head14privacy' 		=>	"2. Datos no personales:",
+
+	'head15privacy'		=>	"Tus opciones:",
+
+  'head16privacy' 		=>  "Exclusiones:",
+
+	'head17privacy' 		=>	"Niños:",
+
+	'head18privacy'		=>	"Enlaces a otros sitios web:",
+
+  'head19privacy' 		=>  "Almacenamiento y seguridad:",
+
+	'head20privacy' 		=>	"Leyes internacionales de privacidad:",
+
+	'head12privacy'		=>	"Programas de Puerto Seguro entre EE. UU. y la U.E. y entre EE. UU. y Suiza:",
+
+  'head13privacy' 		=>  "Otros términos y condiciones:",
+
+	'head14privacy' 		=>	"Cambios en esta Política de privacidad:",
+
+	'head15privacy'		=>	"Acceso a la información:",
+
+  'head16privacy' 		=>  "Conservación y eliminación:",
+
+	'head17privacy' 		=>	"Contacto de SponzorMe:",
+
+	'head18privacy'		=>	"Resolución de disputas:",
+
+
+
+
+);

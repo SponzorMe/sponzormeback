7c210520a8822da20fa94edfe7c0dc1f47af3562
sebastian gomez <seagomezar@gmail.com>
2015-06-30 15:35:36 -0500

    para no tener que autenticar como admin para registrar un usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ use Weblee\Mandrill\Mail;
 class UserController extends Controller {
 	public function __construct()
 	{
-		$this->middleware('auth.basic.once',['only'=>['store','update','destroy']]);
+		$this->middleware('auth.basic.once',['only'=>['update','destroy']]);
 	}
 	/**
 	 * Display a listing of the resource.

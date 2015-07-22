efeb1d48b8d4a7042bd1176ebe5b44f43e1106a8
Sebastian Gomez <seagomezar@gmail.com>
2015-07-21 19:13:55 -0500

    With insert tests ready

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Models\Category;
+use App\Models\User;
+use App\Models\EventType;
+use App\Models\Event;
+use App\Models\Perk;
+use App\Models\Sponzorship;
+use App\Models\InterestCategory;
 
 class PostTest extends TestCase {
 
@@ -88,7 +94,6 @@ class PostTest extends TestCase {
 	public function testPostEventType()
   {
        $faker = Faker\Factory::create();
-			 //var_dump($categories);
        for ($i=0; $i < $this->maxInsertions ; $i++) {
          $eventType=[
             "name" 				=> $faker->unique()->word,
@@ -98,6 +103,146 @@ class PostTest extends TestCase {
     	  $response = $this->call('POST', '/event_types', $eventType, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
     		$this->assertEquals(201, $response->getStatusCode());
         $this->seeInDatabase('event_types', ['name' => $eventType["name"]]);
+      }
+	}
+	public function testEvents()
+  {
+       $faker = Faker\Factory::create();
+			 $users = User::all()->lists('id');
+			 $categories = Category::all()->lists('id');
+			 $eventTypes = EventType::all()->lists('id');
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+         $event=[
+            "title" 						=> $faker->unique()->word,
+						"location" 					=> $faker->word,
+						"starts" 						=> $faker->date($format = 'Y-m-d', $max = 'now'),
+            'ends' 							=> $faker->date($format = 'Y-m-d', $max = 'now'),
+						'image' 						=> $faker->word.".png",
+						'description' 			=> $faker->realText($faker->numberBetween(10,20)),
+						'privacy' 					=> $faker->numberBetween(0,1),
+						'lang' 							=> $this->availableLangs[ceil($faker->numberBetween($min = 0, $max = 2))],
+						'organizer' 				=> $faker->randomElement($users->toArray()),
+						'category' 					=> $faker->randomElement($categories->toArray()),
+						'type' 							=> $faker->randomElement($eventTypes->toArray()),
+            'location_reference'=> $faker->word
+          ];
+    	  $response = $this->call('POST', '/events', $event, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+    		$this->assertEquals(201, $response->getStatusCode());
+        $this->seeInDatabase('events', ['title' => $event["title"]]);
+      }
+	}
+	public function testPerks()
+  {
+       $faker = Faker\Factory::create();
+			 $events = Event::all()->lists('id');
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+         $perk=[
+            "kind" 						=> $faker->unique()->word,
+						"usd" 						=> $faker->numberBetween($min = 100, $max = 999999),
+						"id_event" 				=> $faker->randomElement($events->toArray()),
+            'total_quantity' 	=> $faker->numberBetween($min = 0, $max = 9999)
+          ];
+    	  $response = $this->call('POST', '/perks', $perk, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+    		$this->assertEquals(201, $response->getStatusCode());
+        $this->seeInDatabase('perks', ['kind' => $perk["kind"]]);
+      }
+	}
+	public function testPerkTasks()
+  {
+       $faker = Faker\Factory::create();
+			 $events = Event::all()->lists('id');
+			 $users = User::all()->lists('id');
+			 $perks = Perk::all()->lists('id');
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+         $perkTaks=[
+            "title" 						=> $faker->unique()->word,
+						"description" 			=> $faker->realText($faker->numberBetween(10,20)),
+						"type" 				=> $faker->numberBetween($min = 0, $max = 4),
+            'status' 	=> $faker->numberBetween($min = 0, $max = 4),
+						'user_id' 	=> $faker->randomElement($users->toArray()),
+						'perk_id' 	=> $faker->randomElement($perks->toArray()),
+						'event_id' 	=> $faker->randomElement($events->toArray()),
+          ];
+    	  $response = $this->call('POST', '/perk_tasks', $perkTaks, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+    		$this->assertEquals(201, $response->getStatusCode());
+        $this->seeInDatabase('perk_tasks', ['title' => $perkTaks["title"]]);
+      }
+	}
+	public function testSponzorships()
+  {
+       $faker = Faker\Factory::create();
+			 $events = Event::all()->lists('id');
+			 $users = User::all()->lists('id');
+			 $perks = Perk::all()->lists('id');
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+	         $sponzorship=[
+	            'status' 	=> $faker->numberBetween($min = 0, $max = 4),
+							'sponzor_id' 	=> $faker->randomElement($users->toArray()),
+							'perk_id' 	=> $faker->randomElement($perks->toArray()),
+							'event_id' 	=> $faker->randomElement($events->toArray())
+	          ];
+	    	  $response = $this->call('POST', '/sponzorships', $sponzorship, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+	    		$this->assertEquals(201, $response->getStatusCode());
+	        $this->seeInDatabase('sponzorships',
+					['event_id' => $sponzorship["event_id"],
+					'sponzor_id' => $sponzorship["sponzor_id"],
+					'perk_id' => $sponzorship["perk_id"]]);
+      }
+	}
+	public function testTaskSponzor()
+  {
+       $faker 				= Faker\Factory::create();
+			 $events 				= Event::all()->lists('id');
+			 $users 				= User::all()->lists('id');
+			 $perks 				= Perk::all()->lists('id');
+			 $sponzorships 	= Sponzorship::all()->lists('id');
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+	         $taskSponzor=[
+	            'task_id' 	=> $faker->numberBetween($min = 0, $max = 4),
+							'sponzor_id' 	=> $faker->randomElement($users->toArray()),
+							'perk_id' 	=> $faker->randomElement($perks->toArray()),
+							'organizer_id' 	=> $faker->randomElement($users->toArray()),
+							'event_id' 	=> $faker->randomElement($events->toArray()),
+							'sponzorship_id' 	=> $faker->randomElement($sponzorships->toArray()),
+	          ];
+	    	  $response = $this->call('POST', '/task_sponzor', $taskSponzor, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+	    		$this->assertEquals(201, $response->getStatusCode());
+	        $this->seeInDatabase('task_sponzors',
+					['event_id' => $taskSponzor["event_id"],
+					'sponzor_id' => $taskSponzor["sponzor_id"],
+					'organizer_id' => $taskSponzor["organizer_id"],
+					'perk_id' => $taskSponzor["perk_id"]]);
+      }
+	}
+	public function testUserCategory()
+  {
+       $faker = Faker\Factory::create();
+			 $categories = Category::all()->lists('id');
+			 $users 				= User::all()->lists('id');
+			 //var_dump($categories);
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+         $userCategory=[
+            "user_id" 		=> $faker->randomElement($users->toArray()),
+            'category_id' => $faker->randomElement($categories->toArray())
+          ];
+    	  $response = $this->call('POST', '/user_categories', $userCategory, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+    		$this->assertEquals(201, $response->getStatusCode());
+        $this->seeInDatabase('users_categories', ['user_id' => $userCategory["user_id"],'category_id' => $userCategory["category_id"]]);
+      }
+	}
+	public function testUserInterests()
+  {
+       $faker = Faker\Factory::create();
+			 $interests = InterestCategory::all()->lists('id');
+			 $users 				= User::all()->lists('id');
+       for ($i=0; $i < $this->maxInsertions ; $i++) {
+         $userInterests=[
+            "user_id" 		=> $faker->randomElement($users->toArray()),
+            'interest_id' => $faker->numberBetween($min = 1, $max = count($interests))
+          ];
+    	  $response = $this->call('POST', '/user_interests', $userInterests, array(), array(),array("HTTP_AUTHORIZATION"=>"Basic $this->loginToken"));
+    		$this->assertEquals(201, $response->getStatusCode());
+        $this->seeInDatabase('users_interests', ['user_id' => $userInterests["user_id"],'interest_id' => $userInterests["interest_id"]]);
       }
 	}
 

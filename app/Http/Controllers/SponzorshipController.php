5099328246650f71ce8294e36b304a5dcaf7fc45
Sebastian Gomez <seagomezar@gmail.com>
2015-11-27 12:06:55 -0300

    adding location in sponzorships

--- app/Http/Controllers/SponzorshipController.php
+++ app/Http/Controllers/SponzorshipController.php
@@ -106,7 +106,7 @@ class SponzorshipController extends Controller {
 			->join('events', 'event_id', '=', 'events.id')
 			->join('users', 'sponzor_id', '=', 'users.id')
 			->join('perks', 'perk_id', '=', 'perks.id')
-			->select('users.name','users.email','events.title', 'events.starts', 'events.ends', 'perks.*', 'sponzorships.*')
+			->select('users.name','users.email','events.title', 'events.starts', 'events.location', 'events.ends', 'perks.*', 'sponzorships.*')
 			->get();
 			return response()->json([
 				"success" => true,
@@ -122,7 +122,7 @@ class SponzorshipController extends Controller {
 			->join('events', 'event_id', '=', 'events.id')
 			->join('users', 'organizer_id', '=', 'users.id')
 			->join('perks', 'perk_id', '=', 'perks.id')
-			->select('users.name','users.email','events.title', 'events.starts', 'events.ends', 'perks.*', 'sponzorships.*')
+			->select('users.name','users.email','events.title', 'events.starts', 'events.location', 'events.ends', 'perks.*', 'sponzorships.*')
 			->get();
 			return response()->json([
 				"success" => true,

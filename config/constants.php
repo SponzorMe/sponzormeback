b883d0aa40fe2b7c74f98f620f4da7fdc01e40f2
Sebastian Gomez <seagomezar@gmail.com>
2015-12-10 14:27:51 -0300

    Changin redirect url

--- config/constants.php
+++ config/constants.php
@@ -14,6 +14,6 @@ return array(
 	'activation_url' => "http://app.sponzor.me/#/activation/", //Activation account URL
 	//Used in password controller
 	//'reset_password_url' => "http://localhost:9000/#/reset/", //Reset password URL
-	'reset_password_url' => "http://app.sponzor.me/#/reset/" //Reset password URL
+	'reset_password_url' => "http://app.sponzor.me/#/reset/", //Reset password URL
 	'redirect_evenbrite_front' => "http://localhost:9000/#/eventbrite/" //Reset password URL
 );

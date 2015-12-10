dd514739d07aa7d408fb333701cd9bd7714c6892
Sebastian Gomez <seagomezar@gmail.com>
2015-12-10 14:27:23 -0300

    Changin redirect url

--- config/constants.php
+++ config/constants.php
@@ -15,4 +15,5 @@ return array(
 	//Used in password controller
 	//'reset_password_url' => "http://localhost:9000/#/reset/", //Reset password URL
 	'reset_password_url' => "http://app.sponzor.me/#/reset/" //Reset password URL
+	'redirect_evenbrite_front' => "http://localhost:9000/#/eventbrite/" //Reset password URL
 );

c78bcce66c00cc9fd1c0064dbad063b312a122ea
Sebastian Gomez <seagomezar@gmail.com>
2015-12-11 18:42:13 -0300

    Change

--- config/constants.php
+++ config/constants.php
@@ -12,7 +12,7 @@
 return array(
 	'activation_url' => "http://app.sponzor.me/#/activation/", //Activation account URL
 	'reset_password_url' => "http://app.sponzor.me/#/reset/", //Reset password URL
-	'redirect_evenbrite_front' => "http://staging.sponzor.me/#/eventbrite/",
+	'redirect_evenbrite_front' => "http://carlosrojaso.github.io/sponzormefront/dist/#/eventbrite/",
 	'eventbrite_client_secret'=>"REYYYTW7MW4ABJUI275V3JESPWRR55E5OLKTVC63VNXWFL4WLB",
 	'eventbrite_client_id'=>"6WILTRRV7HVLBSRSGP"
 );

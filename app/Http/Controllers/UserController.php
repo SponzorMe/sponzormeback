aeb3ab8c156aac2af4be463dde6ddd517cb37d34
Sebastian Gomez <seagomezar@gmail.com>
2015-12-14 14:13:51 -0300

    get meetup groups

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -911,6 +911,30 @@ class UserController extends Controller {
 			return response()->json(['Code'=>$code, 'response'=>$response],200);
 		}
 	}
+	public function getMeetupGroups($token){
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => "https://api.meetup.com/2/member/self/?access_token=".$token,
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_HTTPHEADER => array(
+		    "cache-control: no-cache",
+		    "content-type: application/x-www-form-urlencoded"
+		  ),
+		));
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) {
+		  return response()->json(['Code'=>$code, 'response'=>$response],400);
+		} else {
+			return response()->json(['Code'=>$code, 'response'=>$response],200);
+		}
+	}
 	/*Generate bearer token*/
 	public function getEventbriteToken($code){
 		$client_secret = \Config::get('constants.eventbrite_client_secret');

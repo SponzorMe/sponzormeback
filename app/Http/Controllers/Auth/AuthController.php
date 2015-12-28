63ee1c9572832372a64af0aa6185b8eb8ac77e79
Sebastian Gomez <seagomezar@gmail.com>
2015-12-28 11:50:12 -0300

    WIth get rating in login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -16,15 +16,29 @@ class AuthController extends Controller {
     	$email=$request->input("email");
     	$password=$request->input("password");
         $a=Auth::attempt(['email' => $email, 'password' => $password]);
-        if ($a)
-		{
-		    return response()->json([
-				"success" => true,
-				"user"=>Auth::user(),
-                "token"=>Auth::user()->token
-				], 200
-			);
-		}
+        if ($a){
+          if(Auth::user()->type==0){
+    				Auth::user()->rating_like_organizer;
+    				$filtered = Auth::user()->rating_like_organizer->filter(function ($item) {
+    				    return $item->type == 1;
+    				});
+    				$rating = $filtered->avg('question0');
+    			}
+    			else if (Auth::user()->type==1){
+    				Auth::user()->rating_like_sponzor;
+    				$filtered = Auth::user()->rating_like_sponzor->filter(function ($item) {
+    				    return $item->type == 0;
+    				});
+    				$rating = $filtered->avg('question0');
+    			}
+    		  return response()->json([
+    				"success" => true,
+    				"user"=>Auth::user(),
+            "token"=>Auth::user()->token,
+            "rating"=>$rating
+    				], 200
+    			);
+		    }
         else{
         	return response()->json(
 				["message"=>"Invalid credentials",

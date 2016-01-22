a4c233f17240a2a5d732e30bcdd462baf84c0e96
Sebastian Gomez <seagomezar@gmail.com>
2016-01-22 18:28:44 -0300

    Fixing services

--- app/Http/Controllers/UserInterestController.php
+++ app/Http/Controllers/UserInterestController.php
@@ -80,73 +80,27 @@ class UserInterestController extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update(Request $request,$id)
+	public function update(Request $request, $id)
 	{
-		$UserInterest=UserInterest::find($id);
-		if(!$UserInterest){
-			return response()->json(['message'=>"Not found"],404);
-		}
-		else{
-			$inputs = $request->all(); //Get the set of inputs
-			extract($inputs); //Creamos las variables desde el array de inputs
-		}
-		if($request->method()==="PATCH"){//PATCH At least one field is required
-			$warnings=array();
-			$flag=0;//If 0 persist nothing was updated.
-			if(!empty($user_id)){
-				$validator = Validator::make(
-				    ['user_id' => $user_id],
-				    ['user_id' => ['required', 'max:255','exists:users,id']]
-				);
-				if(!$validator->fails()){
-					$flag=1;
-					$UserInterest->user_id=$user_id;
-				}
-				else{
-					$warnings[]=$validator->messages();
-				}
-			}
-			if(!empty($interest_id)){
-				$validator = Validator::make(
-				    ['interest_id' => $interest_id],
-				    ['interest_id' => ['required', 'max:255','exists:interests_categories,id_interest']]
-				);
-				if(!$validator->fails()){
-					$flag=1;
-					$UserInterest->interest_id=$interest_id;
-				}
-				else{
-					$warnings[]=$validator->messages();
-				}
-			}
-			if($flag){
-				$UserInterest->save();
-				return response()->json(['message'=>"Updated",'warnings'=>$warnings,'UserInterest'=>$UserInterest],200);
-			}
-			else{
-				return response()->json(['message'=>"Nothing updated",'warnings'=>$warnings,'UserInterest'=>$UserInterest],200);
-			}
-		}
-		elseif($request->method()==="PUT"){//PUT all fields are required
-			$validation = Validator::make($request->all(), [
-        	'user_id'=>'required|max:11|exists:users,id',
-			'interest_id'=>'required|max:11|exists:interests_categories,id_interest',
-	    	 ]);
+		$all = $request->input("interests");
+		$noInserted = [];
+		$inserted = [];
+		foreach ($all as $a) {
+			$validation = Validator::make($a, [
+      	'user_id'=>'required|max:11|exists:users,id',
+				'interest_id'=>'required|max:11|exists:interests_categories,id_interest'
+	    ]);
 			if($validation->fails())
 			{
-				return response()->json(['message'=>"Not updated",'error'=>$validation->messages()],422);
+				array_push($noInserted, $a);
 			}
 			else
 			{
-				$UserInterest->user_id=$user_id;
-				$UserInterest->interest_id=$interest_id;
-				$UserInterest->save();
-				return response()->json(['message'=>"Updated",'UserInterest'=>$UserInterest],200);
+				UserInterest::create($a);
+				array_push($inserted, $a);
 			}
 		}
-		else{
-			return response()->json(['message'=>"Method Not Allowed"],405);
-		}
+		return response()->json(['inserted'=>$inserted,'noInserted'=>$noInserted],200);
 	}
 	/**
 	 * Remove the specified resource from storage.
